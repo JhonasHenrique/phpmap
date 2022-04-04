464bbdce123ff0c045758e32572dcf76a1c78b84
Jhonas Henrique <Jhonascontato@gmail.com>
2022-04-04 16:43:57 -0300

    PHP MAP ADICIONADO!

---
+++ scanner.php
@@ -0,0 +1,40 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+   <head>
+    <meta charset="utf-8">
+       <title>PHP MAP</title>
+      </head>
+<body bgcolor="black">
+   <hr>
+   <center><p><font color="#00FF00">PHP MAP</font></p></center>
+       <hr>
+          <br>
+ <center><form name="cmd" action="?" method="POST">
+     <font color="#00FF00">Alvo: </font><input type="text" name="host"  placeholder="Digite seu host!" required/>
+     <input type="submit" value="Scan"/>
+      <form></center>
+         <br>
+          <br>
+     <hr>
+        <br>
+     <?php
+      if(isset($_REQUEST['host'])){
+         error_reporting(0);
+         $host = ADDSLASHES($_REQUEST['host']); 
+         $g_name = gethostbyname($host);
+         $lista_p = range(0,80);
+         $arm_p = array();
+         foreach($lista_p as $test_p){
+         $s = socket_create(AF_INET, SOCK_STREAM, 0);
+         if(socket_connect($s, $g_name, $test_p)){
+             array_push($arm_p, $test_p);
+         }
+         }
+        foreach($arm_p as $r){
+            echo "<center>"."<font color='red'>".$r." "."Open!"."</font>".nl2br("\n\n")."</center>";
+         }
+      }
+     ?> 
+      <hr>
+    </body>
+       </html>
